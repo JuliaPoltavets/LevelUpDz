1483234735024c86e95d274e8410d8502e139835
Yuliia Kiselova <yup@sitecore.net>
2016-07-13 01:34:27 +0200

    Snake setup was added

--- ArraysProject/BusinessLayer/MultidimensionalArrays.cs
+++ ArraysProject/BusinessLayer/MultidimensionalArrays.cs
@@ -26,7 +26,44 @@ namespace ArraysProject.BusinessLayer
             return matrixResult;
         }
 
-        public static void SetRandomValues(int[,] matrix,int leftBound = 50, int rightBound=100)
+        public static int[,] SnakeMatrixInit(int[] initData, int rowCount, int colunmCount)
+        {
+            int[,] result = new int[rowCount, colunmCount];
+            SetRandomValues(result, 0, 0);
+            int dataIndex = 0;
+            int currentRow = 0;
+            int currentColumn = 0;
+            result[currentColumn, currentColumn] = initData[dataIndex];
+            ++dataIndex;
+            while ((dataIndex < initData.Length) && (dataIndex < rowCount * colunmCount))
+            {
+                if (currentColumn < result.GetLength(1) - 1)
+                {
+                    SetRight(result, initData, ref dataIndex, ref currentRow, ref currentColumn);
+                }
+                else
+                {
+                    SetDown(result, initData, ref dataIndex, ref currentRow, ref currentColumn);
+                }
+
+                SetDownDiagonal(result, initData, ref dataIndex, ref currentRow, ref currentColumn);
+
+                if (currentRow < result.GetLength(0) - 1)
+                {
+                    SetDown(result, initData, ref dataIndex, ref currentRow, ref currentColumn);
+                }
+                else
+                {
+                    SetRight(result, initData, ref dataIndex, ref currentRow, ref currentColumn);
+                }
+
+                SetUpDiagonal(result, initData, ref dataIndex, ref currentRow, ref currentColumn);
+            }
+
+            return result;
+        }
+
+        public static void SetRandomValues(int[,] matrix, int leftBound = 50, int rightBound=100)
         {
             Random rnd = new Random();
             for (int i = 0; i < matrix.GetLength(0); i++)
@@ -37,5 +74,55 @@ namespace ArraysProject.BusinessLayer
                 }
             }
         }
+
+        #region Private
+        private static void SetRight(int[,] matrix, int[] initData, ref int dataIndex, ref int currentRow, ref int currentColumn)
+        {
+            ++currentColumn;
+            if (dataIndex < initData.Length)
+            {
+                matrix[currentRow, currentColumn] = initData[dataIndex];
+            }
+            ++dataIndex;
+        }
+        private static void SetDownDiagonal(int[,] matrix, int[] initData, ref int dataIndex, ref int currentRow, ref int currentColumn)
+        {
+            while ((currentColumn > 0)&&(currentRow < matrix.GetLength(0)-1))
+            {
+                --currentColumn;
+                ++currentRow;
+                if (dataIndex < initData.Length)
+                {
+                    matrix[currentRow, currentColumn] = initData[dataIndex];
+                    ++dataIndex;
+                }
+            }
+        }
+
+        private static void SetUpDiagonal(int[,] matrix, int[] initData, ref int dataIndex, ref int currentRow, ref int currentColumn)
+        {
+            while ((currentRow > 0) && (currentColumn < matrix.GetLength(1) - 1))
+            {
+                --currentRow;
+                ++currentColumn;
+                if (dataIndex < initData.Length)
+                {
+                    matrix[currentRow, currentColumn] = initData[dataIndex];
+                    ++dataIndex;
+                }
+            }
+        }
+
+        private static void SetDown(int[,] matrix, int[] initData, ref int dataIndex, ref int currentRow, ref int currentColumn)
+        {
+            ++currentRow;
+            if (dataIndex < initData.Length)
+            {
+                matrix[currentRow, currentColumn] = initData[dataIndex];
+            }
+            ++dataIndex;
+        }
+        #endregion
+
     }
 }
--- ArraysProject/Program.cs
+++ ArraysProject/Program.cs
@@ -8,18 +8,22 @@ namespace ArraysProject
     {
         static void Main(string[] args)
         {
-            // Multiplex two matrices
+            //Multiplex two matrices
             int[,] mA = new int[2, 3];
             int[,] mB = new int[3, 5];
-            MultidimensionalArrays.SetRandomValues(mA, -10,10);
-            MultidimensionalArrays.SetRandomValues(mB,-10,10);
+            MultidimensionalArrays.SetRandomValues(mA, -10, 10);
+            MultidimensionalArrays.SetRandomValues(mB, -10, 10);
             PrintArray.PrintMultidimensionalArray(mA);
             PrintArray.PrintMultidimensionalArray(mB);
             int[,] mR = MultidimensionalArrays.MultiplexTwoMatrices(mA, mB);
             PrintArray.PrintMultidimensionalArray(mR);
             Console.ReadLine();
 
-
+            //Snake setup
+            var initArray = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+            var snakeArray = MultidimensionalArrays.SnakeMatrixInit(initArray, 3, 4);
+            PrintArray.PrintMultidimensionalArray(snakeArray);
+            Console.ReadLine();
         }
         
     }
