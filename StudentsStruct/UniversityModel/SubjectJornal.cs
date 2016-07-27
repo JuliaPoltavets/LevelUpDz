d9907b8df9addf9fceef34ab01cc2e7fbc680ea2
JuliaPoltavets <juliapoltavec@gmail.com>
2016-07-27 20:14:28 +0300

    aligned with c# version

--- StudentsStruct/UniversityModel/SubjectJornal.cs
+++ StudentsStruct/UniversityModel/SubjectJornal.cs
@@ -1,32 +1,35 @@
-﻿using System;
+using System;
 
 namespace StudentsStruct.UniversityModel
 {
     public struct SubjectJornal
     {
-        public Subjects SubjectName { get; private set; }
-        public byte[] MarkList { get; private set; }
+        private Subjects _subjectName;
+        private byte[] _markList;
+        public Subjects SubjectName { get { return _subjectName;} }
+        public byte[] MarkList { get { return _markList; } }
 
         public SubjectJornal(Subjects subjectName, byte[] marksList)
         {
-            SubjectName = subjectName;
-            MarkList = marksList;
+            _subjectName = subjectName;
+            _markList = marksList;
         }
 
         public void AddMark(byte mark)
         {
             if (MarkList == null)
             {
-                MarkList = new[] { mark };
+                _markList = new[] { mark };
             }
             else
             {
                 byte[] updatedGroup = new byte[MarkList.Length + 1];
                 Array.Copy(MarkList, 0, updatedGroup, 0, MarkList.Length);
                 updatedGroup[updatedGroup.Length - 1] = mark;
-                MarkList = updatedGroup;
+                _markList = updatedGroup;
             }
         }
+
         public double GetAverageGrade()
         {
             int totalScore = 0;
