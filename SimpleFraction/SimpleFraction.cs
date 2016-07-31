d8ff4b794ad8cc0cf5b1520148984d19756eefb5
Yuliia Kiselova <yup@sitecore.net>
2016-07-31 15:16:43 +0200

    Addition was implemented

--- SimpleFraction/SimpleFraction.cs
+++ SimpleFraction/SimpleFraction.cs
@@ -8,6 +8,21 @@ namespace SimpleFraction
         private int _denominator;
         private int _sign;
 
+        public int Numerator
+        {
+            get { return _numerator; }
+        }
+
+        public int Denominator
+        {
+            get { return _denominator; }
+        }
+
+        public int Sign
+        {
+            get { return _sign; }
+        }
+
         /// <summary>
         /// Construct new SimpleFraction struct
         /// </summary>
@@ -16,7 +31,7 @@ namespace SimpleFraction
         public SimpleFraction(int num, int denom)
         {
             _sign = 1;
-            if (num * denom < 0)
+            if (num*denom < 0)
             {
                 _sign = -1;
             }
@@ -24,6 +39,28 @@ namespace SimpleFraction
             _denominator = denom;
         }
 
+        /// <summary>
+        /// Multiplies current instance of SimpleFraction
+        /// </summary>
+        /// <param name="multiplier"></param>
+        public void MultiplyFraction(int multiplier)
+        {
+            _denominator *= multiplier;
+            _numerator *= multiplier;
+        }
+
+        /// <summary>
+        /// Multiplies particular fraction on the value
+        /// </summary>
+        /// <param name="fraction">fraction to be multiplied</param>
+        /// <param name="multiplier">multiplier value</param>
+        /// <returns>returns new fraction</returns>
+        public static SimpleFraction MultiplyFraction(SimpleFraction fraction, int multiplier)
+        {
+            int newNumerator = fraction.Numerator*multiplier;
+            int newDenominator = fraction.Denominator*multiplier;
+            return new SimpleFraction(fraction.Sign*newNumerator, newDenominator);
+        }
 
         /// <summary>
         /// Calculates greatest common divisor of two numbers
@@ -56,9 +93,46 @@ namespace SimpleFraction
         public static int GetLeastCommonMultiple(int numberA, int numberB)
         {
             int gcdAb = FindGreatestCommonDivisor(numberA, numberB);
-            return numberA * numberB / gcdAb;
+            return numberA*numberB/gcdAb;
         }
 
+        /// <summary>
+        /// Adds two simple fractions 
+        /// </summary>
+        /// <param name="fractionA">first argument</param>
+        /// <param name="fractionB">second argument</param>
+        /// <returns>new simple fraction</returns>
+        public static SimpleFraction Add(SimpleFraction fractionA, SimpleFraction fractionB)
+        {
+            int lcmDenominators = GetLeastCommonMultiple(fractionA.Denominator, fractionB.Denominator);
+            int increaseA = lcmDenominators/fractionA.Denominator;
+            int increaseB = lcmDenominators/fractionB.Denominator;
 
+            fractionA.MultiplyFraction(increaseA);
+            fractionB.MultiplyFraction(increaseB);
+
+            int newNumerator = fractionA.Numerator + fractionB.Numerator;
+            int newDenominator = lcmDenominators;
+            int newSign = fractionA.Sign*fractionB.Sign;
+
+            return new SimpleFraction(newSign*newNumerator, newDenominator);
+        }
+        
+        /// <summary>
+        /// Add new fraction to the current entity of simple fraction
+        /// </summary>
+        /// <param name="fractionN">fraction to be added to the current</param>
+        public void Add(SimpleFraction fractionN)
+        {
+            int lcmDenominators = GetLeastCommonMultiple(_denominator, fractionN.Denominator);
+            int increaseThis = lcmDenominators / _denominator;
+            int increaseN = lcmDenominators / fractionN.Denominator;
+
+            fractionN.MultiplyFraction(increaseN);
+
+            _numerator = _numerator*increaseThis + fractionN.Numerator;
+            _denominator = lcmDenominators;
+            _sign = _sign*fractionN.Sign;
+        }
     }
 }
